2f8ae025a0196226b2e80a1d7418bb8306b6cc59
Fang Liu <fangyiliu@gmail.com>
2021-12-15 19:27:19 -0800

    Register viewsets

--- app/people_depot/urls.py
+++ app/people_depot/urls.py
@@ -15,6 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
