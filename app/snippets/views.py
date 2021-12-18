fa64673ae6dea93163c1a92c71f15627f30e79ab
Fang Liu <fangyiliu@gmail.com>
2021-12-17 20:47:47 -0800

    Convert urlconf to use routers

--- app/snippets/views.py
+++ app/snippets/views.py
@@ -2,9 +2,8 @@ from snippets.permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import action, api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 
 
@@ -35,11 +34,3 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
